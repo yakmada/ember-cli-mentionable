f7b100a69a8940aa27de59e0997c66d51cbf2bf2
yakmada <adam@pbjelli.com>
2017-08-25 00:31:15 -0700

    initial commit gh-pages

--- addon/components/mentionable-textarea.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import mentionableComponent from '../mixins/mentionable-component';
-import layout from '../templates/components/mentionable-textarea';
-
-export default Ember.Component.extend(mentionableComponent, {
-  layout,
-  classNames: ['mentionable-component', 'mentionable-textarea']
-});
--- index.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-'use strict';
-
-module.exports = {
-  name: 'ember-cli-mentionable',
-  included: function(/* app */) {
-    this._super.included.apply(this, arguments);
-  }
-};
