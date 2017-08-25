b9eb36dbddf8ec4ae5a40437b3d46a7dfe39ebac
yakmada <adam@pbjelli.com>
2017-08-24 21:04:29 -0700

    exposing some basic styling which can be imported by consuming app

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-mentionable'
+  name: 'ember-cli-mentionable',
+  included: function(/* app */) {
+    this._super.included.apply(this, arguments);
+  }
 };
